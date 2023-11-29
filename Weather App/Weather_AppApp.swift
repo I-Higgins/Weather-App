3bd17492acaefafe19edd74c36c9b88b3d7bc62d
I-Higgins <lapdog_05_mete@icloud.com>
2023-11-29 14:09:43 +1000

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Isaac Higgins on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
